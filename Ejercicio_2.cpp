71dcb54d3ca943c702ed7f47123873b19a5d03c8
DanielCoduras <43232540+DanielCoduras@users.noreply.github.com>
2019-02-21 08:44:13 +0000

    Add files via upload

---
+++ Ejercicio_2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int num1;
+
+
+
+int main(){
+    int num1;
+    char salir;
+    cout<<"Introduce una cantidad de doladores: ";
+    cin>>num1;
+    
+
+    
+    
+    
+    
+    
+    
+     cout<<"La cantidad en euros son: "<<;
+     cout<<"\nToca cualquier tecla para salir: ";
+     cin>>salir;
+     return 0;
+       
+}
